7ab11bbe14d935801b4df47849c96b7f635bad00
aherath2 <aherath2@illinois.edu>
2018-04-18 23:09:57 -0500

    Got OF gui working

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,17 @@
 #include "view.h"
 #include "controller.h"
 
+#include "ofMain.h"
+
+
+#define DISPLAY_MODE OF_WINDOW
+
 #include <iostream>
 int main() {
     std::cout << "Hello, World" << std::endl;
+
+
+    ofSetupOpenGL(640, 480, DISPLAY_MODE);
+
+	ofRunApp(new ofBaseApp());
 }
